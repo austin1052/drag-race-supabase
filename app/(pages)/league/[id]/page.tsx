6b9963148ccd31feff87e2c7535b48fbac62f545
Adam Reid <>
2023-09-28 19:48:23 -0400

    fixed league import

--- app/(pages)/league/[id]/page.tsx
+++ app/(pages)/league/[id]/page.tsx
@@ -5,13 +5,19 @@ import React, {useState} from "react";
 import Image from 'next/image'
 import {fetcher} from '@/utils/fetcher';
 import { Box, Card, CardContent, CardHeader, Link, Typography } from '@mui/material';
+import { leagueWithInfo } from "@/app/api/league/[id]/route";
+
+
+interface League {
+
+}
 
 export default function Page({ params: {id} }: { params: { id: string } }) {
     const { 
         data: league, 
         error: leagueError, 
         isLoading: leagueIsLoading 
-    } = useSWR<any[]>(`/api/league/${id}`, fetcher);
+    } = useSWR<leagueWithInfo[]>(`/api/league/${id}`, fetcher);
 
     if (leagueIsLoading) return <div>Loading...</div>;
     if (!league || leagueError) return <pre>{JSON.stringify(leagueError)}</pre>;
--- app/api/league/[id]/route.ts
+++ app/api/league/[id]/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse, NextRequest } from 'next/server';
+import {Prisma} from '@prisma/client';
 import prisma from '@/prisma/client';
 
 export async function GET(req: NextRequest, params: { id: string }) {
@@ -27,3 +28,35 @@ export async function GET(req: NextRequest, params: { id: string }) {
     });
     return NextResponse.json(data);
 }
+
+const prismaLeagueArgs = {
+    include: {
+        team: {
+            include: {
+                app_user: true, 
+                draft_position: {
+                    include: {
+                        queen: {
+                            include: {
+                                scoring_event: {
+                                    include: {event_type: true}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        season: {
+            include: {queen: true}
+        }
+    }
+};
+
+function getLeague() {
+    return prisma.league.findMany(prismaLeagueArgs);
+}
+
+const leagueWithInfo = Prisma.validator<Prisma.leagueDefaultArgs>()(prismaLeagueArgs)
+
+export type leagueWithInfo = Prisma.leagueGetPayload<typeof leagueWithInfo>
