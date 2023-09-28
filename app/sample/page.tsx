d253dd05006a5ad826bfd82a6a59740b0b79b803
Adam Reid <>
2023-09-27 23:04:19 -0400

    sample my-leagues page is working

--- app/sample/page.tsx
+++ app/sample/page.tsx
@@ -2,7 +2,9 @@
 
 import useSWR from "swr";
 import React, {useState} from "react";
+import Image from 'next/image'
 import {fetcher} from '@/utils/fetcher';
+import { Box, Card, CardContent, CardHeader, Typography } from '@mui/material';
 
 export default function Page() {
     const { 
@@ -14,14 +16,37 @@ export default function Page() {
     if (leaguesIsLoading) return <div>Loading...</div>;
     if (leaguesError) return <pre>{leaguesError}</pre>;
 
+    console.log(leagues);
+
     return (
         <div>
-            <h1>Page</h1>
-            <ul>
-                {leagues?.map((league) => (
-                    <li key={league.id}>{league.name}</li>
-                ))}
-            </ul>
+            <h1>My Leagues</h1>
+            <Box>
+                {leagues?.map((league) => {
+                    console.log(league);
+                    return (
+                        <Card sx={{ width: 350}} key={league.id} style={{backgroundColor: 'lightgrey'}}>
+
+                            <CardContent>
+                                <Box alignItems="center">
+                                    <Typography variant="h5" color="text.secondary" gutterBottom textAlign={"center"}>
+                                        {league.name}
+                                    </Typography>
+                                </Box>
+                                <Box alignItems={"center"}>
+                                    <Image 
+                                        src={league.banner_url} 
+                                        alt={league.name}  
+                                        width={300}
+                                        height={150}                    
+                                    />
+                                </Box>
+
+                            </CardContent>
+                        </Card>
+                    )
+                })}
+            </Box>
         </div>
 
     )
